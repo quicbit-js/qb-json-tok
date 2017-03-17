5c4420f739a3b129c6c071c42a90ecc69647140d
mvoss9000 <mvoss9000@yahoo.com>
2017-03-17 12:27:50 -0400

    format/lint/fix

--- perf/ptest.js
+++ perf/ptest.js
@@ -11,50 +11,48 @@ var utf8 = require('qb-utf8-ez')
 //
 //
 var json = {
-    generate: function gen_json(str_value, buf_size) {
-        var sample_buf = utf8.buffer('"' + str_value + '"')
-        var slen = sample_buf.length
-        var vals = []
-        for(var rem = buf_size-8; rem >= slen; rem -= (slen+1)) {   // -8 for {v:[...]) +1 for first, +1 for commas after
-            vals.push(sample_buf)
-        }
-        if(rem > 1) {       // need at least 2 bytes to quote
-            var part = utf8.escape_illegal(sample_buf.slice(0, rem))
-            part[0] = 34
-            part[part.length-1] = 34
-            vals.push(part)
-        }
-        return utf8.join([
-            utf8.buffer('{"v":['),
-            utf8.join(vals, ','),
-            utf8.buffer(']}')
-        ], '')
+  generate: function gen_json (str_value, buf_size) {
+    var sample_buf = utf8.buffer('"' + str_value + '"')
+    var slen = sample_buf.length
+    var vals = []
+    for (var rem = buf_size - 8; rem >= slen; rem -= (slen + 1)) {   // -8 for {v:[...]) +1 for first, +1 for commas after
+      vals.push(sample_buf)
     }
+    if (rem > 1) {       // need at least 2 bytes to quote
+      var part = utf8.escape_illegal(sample_buf.slice(0, rem))
+      part[0] = 34
+      part[part.length - 1] = 34
+      vals.push(part)
+    }
+    return utf8.join([
+      utf8.buffer('{"v":['),
+      utf8.join(vals, ','),
+      utf8.buffer(']}')
+    ], '')
+  }
 }
 
-function time_tokenize(sample, buf_size, total_bytes) {
-    var utf8 = require('qb-utf8-ez')
-    var buf = json.generate(sample, buf_size)
-    console.log('buf size:', buf.length)
-    var times =  total_bytes / buf_size
-    var encoded
-    var keys = 0, vals = 0
-    var cb = function(buf, ki, klen,tok, vi, vlen) {
-        if(ki !== -1) keys++
-        vals++
-    }
-    var t0 = performance.now()
-    for(var n=0; n<times; n++) {
-        tokenize(buf, cb)
-    }
-    console.log(keys, 'keys', vals, 'vals')
-    var ms = performance.now() - t0
-    var mbs = total_bytes / (ms * 1000)
-    console.log('tokenize: ' + (buf_size/1000) + 'kb buffers ' + times + ' times in ' + ms + ' ms.  ' +  mbs + ' mb per sec.')
+function time_tokenize (sample, buf_size, total_bytes) {
+  var buf = json.generate(sample, buf_size)
+  console.log('buf size:', buf.length)
+  var times =  total_bytes / buf_size
+  var keys = 0, vals = 0
+  var cb = function (buf, ki, klen,tok, vi, vlen) {
+    if (ki !== -1) keys++
+    vals++
+  }
+  var t0 = performance.now()
+  for (var n = 0; n < times; n++) {
+    tokenize(buf, cb)
+  }
+  console.log(keys, 'keys', vals, 'vals')
+  var ms = performance.now() - t0
+  var mbs = total_bytes / (ms * 1000)
+  console.log('tokenize: ' + (buf_size / 1000) + 'kb buffers ' + times + ' times in ' + ms + ' ms.  ' +  mbs + ' mb per sec.')
 }
 
-if(typeof window !== 'undefined') {
-    var ptest = window.document.ptest = Object.assign({}, window.document.ptest, {
-        time_tokenize: time_tokenize
-    })
+if (typeof window !== 'undefined') {
+  window.document.ptest = Object.assign({}, window.document.ptest, {
+    time_tokenize: time_tokenize
+  })
 }
