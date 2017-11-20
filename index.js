8c35974bdb3a6f189644471f45f9ba386fce37bf
mvoss9000 <mvoss9000@yahoo.com>
2017-11-20 13:09:37 -0500

    overhaul - prelim

--- index.js
+++ index.js
@@ -1,3 +1,90 @@
+// STATES   - use second byte.  LSB is for TOK / ascii
+var CTX_MASK =    0x0300
+var CTX_UNK =     0x0000    // unknown context - need to check stack
+var CTX_OBJ =     0x0100
+var CTX_ARR =     0x0200
+var CTX_NONE =    0x0300
+
+var POS_MASK =    0x0C00
+var BEFORE =      0x0400
+var AFTER =       0x0800
+var INSIDE =      0x0C00
+
+var KEYVAL_MASK = 0x1000
+var VAL =         0x0000
+var KEY =         0x1000
+
+var FIRST =       0x2000     // is first value in an object or array
+
+var NUM_CHARS = '-0123456789'      // legal number start chars
+var VAL_CHARS = '"ntf' + NUM_CHARS
+
+// create an int-int map from (state + tok) -- to --> (new state)
+function state_map () {
+  var ret = []
+
+  // for each char: map (state0 + ascii) -> state1
+  var map = function (s0, chars, s1) {
+    for (var i=0; i<chars.length; i++) {
+      ret[s0 | chars.charCodeAt(i)] = s1
+    }
+  }
+
+  // start array
+  map( CTX_NONE | BEFORE|FIRST|VAL, '[',        CTX_ARR | BEFORE|FIRST|VAL )
+  map( CTX_ARR  | BEFORE|FIRST|VAL, '[',        CTX_ARR | BEFORE|FIRST|VAL )
+  map( CTX_OBJ  | BEFORE|FIRST|VAL, '[',        CTX_ARR | BEFORE|FIRST|VAL )
+  map( CTX_NONE | BEFORE|VAL,       '[',        CTX_ARR | BEFORE|FIRST|VAL )
+  map( CTX_ARR  | BEFORE|VAL,       '[',        CTX_ARR | BEFORE|FIRST|VAL )
+  map( CTX_OBJ  | BEFORE|VAL,       '[',        CTX_ARR | BEFORE|FIRST|VAL )
+
+  // start object
+  map( CTX_NONE | BEFORE|FIRST|VAL, '{',        CTX_OBJ | BEFORE|FIRST|KEY )
+  map( CTX_ARR  | BEFORE|FIRST|VAL, '{',        CTX_OBJ | BEFORE|FIRST|KEY )
+  map( CTX_OBJ  | BEFORE|FIRST|VAL, '{',        CTX_OBJ | BEFORE|FIRST|KEY )
+  map( CTX_NONE | BEFORE|VAL,       '{',        CTX_OBJ | BEFORE|FIRST|KEY )
+  map( CTX_ARR  | BEFORE|VAL,       '{',        CTX_OBJ | BEFORE|FIRST|KEY )
+  map( CTX_OBJ  | BEFORE|VAL,       '{',        CTX_OBJ | BEFORE|FIRST|KEY )
+
+  // values (no context)
+  map( CTX_NONE | BEFORE|FIRST|VAL, VAL_CHARS, CTX_NONE | AFTER|VAL )
+  map( CTX_NONE | AFTER|VAL,        ',',       CTX_NONE | BEFORE|VAL )
+  map( CTX_NONE | BEFORE|VAL,       VAL_CHARS, CTX_NONE | AFTER|VAL )   // etc ...
+
+  // array values
+  map( CTX_ARR | BEFORE|FIRST|VAL,  VAL_CHARS,  CTX_ARR | AFTER|VAL )
+  map( CTX_ARR | AFTER|VAL,         ',',        CTX_ARR | BEFORE|VAL )
+  map( CTX_ARR | BEFORE|VAL,        VAL_CHARS,  CTX_ARR | AFTER|VAL )   // etc ...
+
+  // object fields
+  map( CTX_OBJ | BEFORE|FIRST|KEY,  '"',        CTX_OBJ | AFTER|KEY )
+  map( CTX_OBJ | AFTER|KEY,         ':',        CTX_OBJ | BEFORE|VAL )
+  map( CTX_OBJ | BEFORE|VAL,        VAL_CHARS,  CTX_OBJ | AFTER|VAL )
+  map( CTX_OBJ | AFTER|VAL,         ',',        CTX_OBJ | BEFORE|KEY )
+  map( CTX_OBJ | BEFORE|KEY,        ':',        CTX_OBJ | BEFORE|VAL )  // etc ...
+
+  // end contexts - check stack for next context
+  map( CTX_ARR | BEFORE|FIRST|VAL,  ']',        CTX_UNK | AFTER|VAL )   // empty array
+  map( CTX_ARR | AFTER|VAL,         ']',        CTX_UNK | AFTER|VAL )
+  map( CTX_OBJ | BEFORE|FIRST|KEY,  '}',        CTX_UNK | AFTER|VAL )   // empty object
+  map( CTX_OBJ | BEFORE|KEY,        '}',        CTX_UNK | AFTER|VAL )
+
+  return ret
+}
+
+var STATES = state_map()
+
+function err (msg) { throw Error(msg) }
+
+function map_ascii (s, code) {
+  var ret = []
+  for (var i=0; i<s.length; i++) { ret[s.charCodeAt(i)] = code }
+  return ret
+}
+
+var WHITESPACE = map_ascii('\n\t\r\f', 1)
+// var NUM_CHARS_ALL = NUM_CHARS + '+.eE'   // all legal number chars
+
 function tokenize (cb, src, off, lim) {
   off = off || 0
   lim = lim == null ? src.length : lim
@@ -5,177 +92,107 @@ function tokenize (cb, src, off, lim) {
   var idx = off           // current index offset into buf
   var koff = -1           
   var klim = -1
-  var tok = 0             // current token being handled
+  var tok = -1             // current token being handled
   var voff = -1           // value start index
   var info = null         // extra information about errors or split values
-  var prev_tok = -1
   var stack = []          // collection of array and object open braces (for checking matched braces)
-  var in_what = 0         // 0 = nothing, 91 = array, 123 = object (same as starting ascii code)
+  var state = CTX_NONE|BEFORE|VAL   // see state_map()
+  var pstate = 0                    // previous state
 
-  main_loop: while (idx < lim) {
+  while (idx < lim && WHITESPACE[src[idx]]) { idx++ }
+
+  while (idx < lim) {
     voff = -1
     info = null
-    prev_tok = tok
     tok = src[idx]
-    tok_switch: switch (tok) {
-      case 9:             // TAB
-      case 10:            // NL
-      case 13:            // CR
-      case 32:            // SPACE
-        while (++idx < lim) {
-          switch (src[idx]) {
-            case 9:                     // TAB
-            case 10:                    // NL
-            case 13:                    // CR
-            case 32:                    // SPACE
-              continue
-            default:
-              tok = prev_tok                // whitespace is not a token
-              continue main_loop
-          }
-        }
-        tok = prev_tok                      // whitespace is not a token
-        continue
-      case 34:                              // "    QUOTE
-        voff = idx
-        // console.log('string at ', vi)
-        while (true) {
-          while (src[++idx] !== 34) {
-            if (idx === lim) {
-              info = {msg: 'unterminated string' }
-              tok = 0
-              break tok_switch
+    pstate = state
+    state = STATES[pstate + tok]
+    if (state === undefined) {
+      info = {msg: 'unexpected character'}
+      tok = 0
+    } else {
+      tok_switch: switch (tok) {
+        case 34:                              // "    QUOTE
+          voff = idx
+          // console.log('string at ', vi)
+          while (true) {
+            while (src[++idx] !== 34) {
+              if (idx === lim) {
+                info = {msg: 'unterminated string' }
+                tok = 0
+                break tok_switch
+              }
+            }
+            for (var i=1; src[idx - i] === 92; i++) {}  // \    count BACKSLASH
+            if (i % 2) {
+              break
             }
           }
-          for (var i=1; src[idx - i] === 92; i++) {}  // \    count BACKSLASH
-          if (i % 2) {
-            break
+          idx++       // move past end quote
+
+          if (pstate & (POS_MASK | KEYVAL_MASK) === (BEFORE | KEY)) {
+            koff = voff
+            klim = idx
+            voff = -1
+            continue  // main_loop: next tokens could be :-and-value or something else
           }
-        }
-        idx++       // move past end quote
-        if (koff === -1) {
-          // set string index (potential key)
-          koff = voff
-          voff = -1
-          klim = idx
-          continue  // main_loop: next tokens could be :-and-value or something else
-        }
-        break
-      case 91:                              // [    ARRAY START
-        in_what = 91
-        voff = idx++
-        stack.push(tok)
-        break
-      case 123:                             // {    OBJECT START
-        in_what = 123
-        voff = idx++
-        stack.push(tok)
-        break
-      case 93:                              // ]    ARRAY END
-        if (stack.pop() !== 91) {
-          info = {msg: 'unexpected array end'}
-          tok = 0
-        }
-        in_what = stack.length === 0 ? 0 : stack[stack.length-1]
-        voff = idx++
-        break
-      case 125:                             // }    OBJECT END
-        if (stack.pop() !== 123) {
-          info = {msg: 'unexpected object end'}
-          tok = 0
-        }
-        in_what = stack.length === 0 ? 0 : stack[stack.length-1]
-        voff = idx++
-        break
-      case 58:                              // :    COLON
-        if (koff === -1) {
-          info = {msg: 'unexpected colon' }
-          tok = 0
-          voff = idx++
           break
-        } else {
+        case 91:                                  // [    ARRAY START
+        case 123:                                 // {    OBJECT START
+          voff = idx++
+          stack.push(tok)
+          break
+        case 93:                                  // ]    ARRAY END
+        case 125:                                 // }    OBJECT END
+          voff = idx++
+          stack.pop()
+          (state & CTX_MASK) === CTX_NONE || err('bad context')
+          if (stack.length > 0) {
+            state |= stack[stack.length-1] === 91 ? CTX_ARR : CTX_OBJ   // CTX_NONE is zero
+          }
+          break
+        case 44:                                  // ,    COMMA
+        case 58:                                  // :    COLON
           idx++
           continue
-        }
-      case 110:                                 // n  null
-      case 116:                                 // t  true
-        voff = idx
-        idx += 4
-        break
-      case 102:                                 // f  false
-        voff = idx
-        idx += 5
-        break
-      case 48:case 49:case 50:case 51:case 52:   // digits 0-4
-      case 53:case 54:case 55:case 56:case 57:   // digits 5-9
-      case 45:                                   // '-'   ('+' is not legal here)
-        voff = idx
-        tok = 78                                 // N  Number
-        while (++idx < lim) {
-          switch (src[idx]) {
-            // skip all possibly-valid characters - as fast as we can
-            case 48:case 49:case 50:case 51:case 52:   // digits 0-4
-            case 53:case 54:case 55:case 56:case 57:   // digits 5-9
-            case 43:                                   // +
-            case 45:                                   // -
-            case 46:                                   // .
-            case 69:                                   // E
-            case 101:                                  // e
-              break
-            default:
-              break tok_switch
-          }
-        }
-        break
-      case 44:                                // COMMA
-          // console.log('comma at ', idx)
-        switch (prev_tok) {
-            case 34:                              // "    QUOTE (string)
-            case 78:                              // N    NUMBER
-            case 93:                              // ]    ARRAY END
-            case 102:                             // f    false
-            case 110:                             // n    null
-            case 116:                             // t    true
-            case 125:                             // }    OBJECT END
-                idx++
+        case 110:                                 // n    null
+        case 116:                                 // t    true
+          voff = idx
+          idx += 4
+          break
+        case 102:                                 // f    false
+          voff = idx
+          idx += 5
+          break
+        case 48:case 49:case 50:case 51:case 52:   // digits 0-4
+        case 53:case 54:case 55:case 56:case 57:   // digits 5-9
+        case 45:                                   // '-'   ('+' is not legal here)
+          voff = idx
+          tok = TOK.NUM                                 // N  Number
+          while (++idx < lim) {
+            switch (src[idx]) {
+              // skip all possibly-valid characters - as fast as we can
+              case 48:case 49:case 50:case 51:case 52:   // digits 0-4
+              case 53:case 54:case 55:case 56:case 57:   // digits 5-9
+              case 43:                                   // +
+              case 45:                                   // -
+              case 46:                                   // .
+              case 69:                                   // E
+              case 101:                                  // e
                 break
-            default:
-                info = {msg: 'unexpected comma' }
-                tok = 0
-                voff = idx++
+              default:
                 break tok_switch
-        }
-        continue
-      default:
-        voff = idx++
-        info = {msg: 'unexpected character' }
-        tok = 0
-    }
-    var cbres = -1
-    if (koff === -1) {
-      // non-string
-      cbres = cb(src, -1, 0, tok, voff, idx - voff, info)
-    } else {
-      // string
-      if (prev_tok === 58) {                              // COLON
-        // string with-preceding-colon
-        cbres = cb(src, koff, klim, tok, voff, idx - voff, info)
-      } else {
-        // string no-preceding-colon
-        cbres = cb(src, -1, 0, 34, koff, (klim - koff))              // 34 STRING (QUOTE)
-        if (cbres > 0) {
-          // reset state (prev_tok and vi are reset in main_loop)
-          tok = 0; idx = cbres; koff = -1; klim = -1; continue         // cb requested index
-        } else if (cbres === 0) {
-          return                                                    // cb requested stop
-        }
-        // in valid JSON vi cannot be a key because:
-        // { string:string, string:string, ... } are consumed in pairs
-        cbres = cb(src, -1, 0, tok, voff, idx - voff, info)
+            }
+          }
+          break
+        default:
+          voff = idx++
+          info = {msg: 'unexpected character' }
+          tok = 0
       }
-      koff = -1; klim = -1
     }
 
+    var cbres = cb(src, -1, 0, tok, voff, idx - voff, info)
     if (cbres > 0) {
       // reset state (prev_tok and vi are reset in main_loop)
       tok = 0; idx = cbres; koff = -1; klim = -1                       // cb requested index
@@ -184,33 +201,37 @@ function tokenize (cb, src, off, lim) {
     }
   }  // end main_loop: while(idx < lim) {...
 
-    if (koff !== -1) {
-        cb(src, -1, 0, 34, koff, (klim - koff))  // push out pending string (34 = QUOTE) as a value
-    }
-    cb(src, -1, 0, 69, idx, 0)      // 'E' - end
+  // handle this when implementing split handling
+  // if (koff !== -1) {
+  //     cb(src, -1, 0, 34, koff, (klim - koff))  // push out pending string
+  // }
+  cb(src, -1, 0, TOK.END, idx, 0)
 }
 
-tokenize.TOK = {
-  STR: 34,        // '"'
-  END: 69,        // 'E' - buffer limit reached
-  NUM: 78,        // 'N'
+var TOK = {
   ARR_BEG: 91,    // '['
   ARR_END: 93,    // ']'
+  OBJ_BEG: 123,   // '{'
+  OBJ_END: 125,   // '}'
+  STR: 34,        // '"'
   FAL: 102,       // 'f'
   NUL: 110,       // 'n'
   TRU: 116,       // 't'
-  OBJ_BEG: 123,   // '{'
-  OBJ_END: 125,   // '}'
   ERR: 0,         // error.  check err_info for information
+  END: 69,        // 'E' - buffer limit reached
+  NUM: 78,        // 'N'
 }
 
-tokenize.WHERE = {
-  BEFORE_KEY:     'before_key',  // before an object key was started (before the start quote)
-  IN_KEY:         'in_key',      // inside an object key (before the end quote)
-  AFTER_KEY:      'after_key',   // after an object key, but before the colon ':'
-  BEFORE_VAL:     'before_val',  // before an object or array value (after the comma, colon, or starting array brace
-  IN_VAL:         'in_val',      // inside an object or array value (includes uncertain number cases like 12.3<end>)
-  AFTER_VAL:      'after_val',   // after an object or array value, but before the comma or closing array or object brace
+// where codes combine with
+var WHERE = {
+  BEFORE_KEY:      0x100,    // before an object key was started (before the start quote)
+  IN_KEY:          0x200,    // inside an object key (before the end quote)
+  AFTER_KEY:       0x300,    // after an object key, but before the colon ':'
+  BEFORE_VAL:      0x400,    // before an object or array value (after the comma, colon, or starting array brace
+  IN_VAL:          0x800,    // inside an object or array value (includes uncertain number cases like 12.3<end>)
+  AFTER_VAL:      0x1000,    // after an object or array value, but before the comma or closing array or object brace
 }
+tokenize.WHERE = WHERE
+tokenize.TOK = TOK
 
 module.exports = tokenize
